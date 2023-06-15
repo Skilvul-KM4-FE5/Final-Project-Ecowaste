4b9858bf80847b6f863ff8b29b7c8bfe595e3f80
Viona Cornelia <vionacornelia12@gmail.com>
2023-06-15 16:45:43 +0700

    create routes for kategori sampah

--- src/main.jsx
+++ src/main.jsx
@@ -11,11 +11,19 @@ import {
 } from "react-router-dom";
 import Layout from "./Layout";
 import Home from "./pages/Home";
+import PilahSampah from "./pages/PilahSampah";
+import SampahOrganik from "./pages/SampahOrganik";
+import SampahAnorganik from "./pages/SampahAnorganik";
+import SampahB3 from "./pages/SampahB3";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route index element={<Home />} />
+      <Route path="pilah-sampah" element={<PilahSampah />} />
+      <Route path="sampah-organik" element={<SampahOrganik />} />
+      <Route path="sampah-anorganik" element={<SampahAnorganik />} />
+      <Route path="sampah-B3" element={<SampahB3 />} />
     </Route>
   )
 );
