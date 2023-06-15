90bd0ef96646ec3fb93da3664445bcb14f61eb5e
Wesly Daud <weslydaud02@gmail.com>
2023-06-15 19:13:33 +0700

    update code

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,32 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
-
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
 import Layout from "./Layout";
 import Home from "./pages/Home";
+import BeritaPage from "./pages/Beritapage";
+import DetailBeritaPage from "./pages/DetailBerita";
+import LoginPage from "./pages/LoginPage";
+import Register from "./pages/RegisterPage";
+import { AuthProvider } from "./context/AuthContext";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route index element={<Home />} />
+      <Route path="Berita" element={<BeritaPage />} />
+      <Route path="/DetailBerita/:newsId" element={<DetailBeritaPage />} />
+      <Route path="login" element={<LoginPage />} />
+      <Route path="register" element={<Register />} />
     </Route>
   )
 );
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>
 );
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,52 @@
+import React, { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { MDBContainer, MDBInput, MDBCheckbox, MDBBtn, MDBIcon, MDBCol } from "mdb-react-ui-kit";
+import { AuthContext } from "../context/AuthContext";
+import Swal from "sweetalert2";
+
+function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { login } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const success = await login(email, password);
+    if (success) {
+      Swal.fire({
+        position: "center",
+        icon: "success",
+        title: "Your successful login",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+      navigate("/");
+      return;
+    }
+  };
+
+  return (
+    <MDBContainer className="p-3 my-5 d-flex flex-column w-50">
+      <MDBCol className="d-flex justify-content-center">
+        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp" className="img-fluid" alt="Sample image" style={{ width: "400px", height: "auto", paddingBottom: "1.5rem" }} />
+      </MDBCol>
+
+      <MDBInput wrapperClass="mb-4" label="Email address" value={email} id="form1" type="email" onChange={(e) => setEmail(e.target.value)} />
+      <MDBInput wrapperClass="mb-4" label="Password" value={password} id="form2" type="password" onChange={(e) => setPassword(e.target.value)} />
+
+      <MDBBtn className="mb-4" onClick={handleSubmit}>
+        Sign in
+      </MDBBtn>
+
+      <div className="text-center">
+        <p>
+          Not a member? <Link to="/register">Register</Link>
+        </p>
+      </div>
+    </MDBContainer>
+  );
+}
+
+export default LoginPage;
