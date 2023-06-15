68070ae154793ec164c124b25ba069f99e465deb
Viona Cornelia <vionacornelia12@gmail.com>
2023-06-15 19:57:08 +0700

    update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import {
@@ -11,17 +10,24 @@ import {
 } from "react-router-dom";
 import Layout from "./Layout";
 import Home from "./pages/Home";
+import BeritaPage from "./pages/Beritapage";
+import DetailBeritaPage from "./pages/DetailBerita";
+import { AuthProvider } from "./context/AuthContext";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route index element={<Home />} />
+      <Route path="Berita" element={<BeritaPage />} />
+      <Route path="/DetailBerita/:newsId" element={<DetailBeritaPage />} />
     </Route>
   )
 );
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>
 );
