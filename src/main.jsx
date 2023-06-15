1865d8e73b578d9a61a2712bc8d7dee71a515f9b
Viona Cornelia <vionacornelia12@gmail.com>
2023-06-15 16:01:06 +0700

    update main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,27 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Layout from "./Layout";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
