3f8ec17d489edd0270e2d08179600e24242fc5ad
Wesly Daud <weslydaud02@gmail.com>
2023-06-15 17:33:52 +0700

    add section const detail berita

---
+++ src/components/DetailBerita.jsx
@@ -0,0 +1,30 @@
+// @ts-nocheck
+import { useContext, useEffect, useState } from "react";
+
+import { useParams } from "react-router-dom";
+
+import { AuthContext } from "../context/AuthContext";
+
+const url = "https://644e26454e86e9a4d8f1fcf6.mockapi.io/api/v1/berita/";
+
+const DetailBerita = () => {
+  const param = useParams();
+
+  const [berita, setBerita] = useState([]);
+
+  const { isLoggedIn } = useContext(AuthContext);
+
+  const getDataBerita = async () => {
+    const response = await fetch(url + param.newsId);
+    const dataBerita = await response.json();
+    setBerita(dataBerita);
+  };
+
+  useEffect(() => {
+    getDataBerita();
+  }, []);
+
+  return <></>;
+};
+
+export default DetailBerita;
