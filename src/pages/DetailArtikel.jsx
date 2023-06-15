d0615be97894a4a425472cd5f6bd79db17874fb8
ErlanggaGuntur10 <erlanggaguntur45@gmail.com>
2023-06-15 17:45:12 +0700

    add breadcrumbs

--- src/pages/DetailArtikel.jsx
+++ src/pages/DetailArtikel.jsx
@@ -4,6 +4,25 @@ export default function DetailArtikel() {
   return (
     <>
       <main id="main">
+      <div className="breadcrumbs">
+        <nav>
+          <div className="container">
+            <ol>
+              <li>
+                <Link className="" to="/">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link className="" to="/artikel">
+                  Artikel
+                </Link>
+              </li>
+              <li>Detail Artikel</li>
+            </ol>
+          </div>
+        </nav>
+      </div>
       
     </main>
     </>
