f66f84d56b6921d625d20d8a47bda79f785e9fb3
ErlanggaGuntur10 <erlanggaguntur45@gmail.com>
2023-06-15 17:14:39 +0700

    add breadcrumbs

--- src/pages/ArtikelPage.jsx
+++ src/pages/ArtikelPage.jsx
@@ -1,8 +1,28 @@
-export default function ArtikelPage() {
-    return (
-      <div>
-        <h1>artikel</h1>
+import { Link } from "react-router-dom";
+
+export default function Artikel() {
+  return (
+    <>
+      <main id="main">
+      <div className="breadcrumbs">
+        <nav>
+          <div className="container">
+            <ol>
+              <li>
+                <Link className="" to="/">
+                  Home
+                </Link>
+              </li>
+              <li>
+                  <Link to="/artikel">Blog</Link>
+             </li>
+              <li>Artikel</li>
+            </ol>
+          </div>
+        </nav>
       </div>
-    );
-  }
-  
+      </main>
+    </>
+    
+  );
+}
